f0edd619b116cd31272654e40d6cfa6d341003c8
Pool Petter Hijuela Florian <poolpetterhijuel@hotmail.com>
2023-09-16 09:44:46 -0500

    feat: initialize project (backend) with spring initialzr

---
+++ src/main/java/io/github/popehiflo/busseatbooker_backend/BusseatbookerBackendApplication.java
@@ -0,0 +1,13 @@
+package io.github.popehiflo.busseatbooker_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusseatbookerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusseatbookerBackendApplication.class, args);
+	}
+
+}
