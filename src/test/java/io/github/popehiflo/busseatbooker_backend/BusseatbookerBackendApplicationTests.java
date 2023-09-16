7c8414e61c442c6a7397781955714bb1b30635fb
Pool Petter Hijuela Florian <poolpetterhijuel@hotmail.com>
2023-09-16 10:37:52 -0500

    base project (#1)
    * feat: initialize project (backend) with spring initialzr
    
    * feat: project structure

---
+++ src/test/java/io/github/popehiflo/busseatbooker_backend/BusseatbookerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.popehiflo.busseatbooker_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusseatbookerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
